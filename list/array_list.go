cecbc9dc780da53ffa579e58de686f0d519741b6
WJX2001 <1758807220@qq.com>
2024-10-18 20:12:39 +0800

    feat: ArrayList实现

---
+++ list/array_list.go
@@ -0,0 +1,90 @@
+package list
+
+import (
+	"github.com/dataStrcut-tool/internal/errs"
+	"github.com/dataStrcut-tool/internal/slice"
+)
+
+var (
+	_ List[any] = &ArrayList[any]{}
+)
+
+// ArrayList 基于切片的简单封装
+type ArrayList[T any] struct {
+	vals []T
+}
+
+// a代表调用GET方法的实例
+func (a *ArrayList[T]) Get(index int) (t T, e error) {
+	l := a.Len()
+	if index < 0 || index >= l {
+		return t, errs.NewErrIndexOutOfRange(l, index)
+	}
+	return a.vals[index], nil
+}
+
+// Append 往ArrayList中追加元素
+func (a *ArrayList[T]) Append(ts ...T) error {
+	a.vals = append(a.vals, ts...)
+	return nil
+}
+
+// Add 在ArrayList中下标为index的位置插入一个元素
+// 当index等于ArrayList的长度时候，相当于Append
+func (a *ArrayList[T]) Add(index int, t T) (err error) {
+	a.vals, err = slice.Add(a.vals, t, index)
+	return
+}
+
+// Set 设置ArrayList里index位置的值为t
+func (a *ArrayList[T]) Set(index int, t T) error {
+	length := len(a.vals)
+	if index >= length || index < 0 {
+		return errs.NewErrIndexOutOfRange(length, index)
+	}
+	a.vals[index] = t
+	return nil
+}
+
+// Delete 方法会在必要的时候引起缩容，其缩容规则是
+// - 如果容量 > 2048，并且长度小于容量一半，那么就会缩容为原来的 5/8
+// - 如果容量 (64, 2048], 如果长度是容量的 1/4，那么就会缩容为原本的一半
+// - 如果此时容量 <= 64,那么我们将不会执行缩容。在容量很小的情况下，浪费的内存很少，没必要消耗 CPU去执行缩容
+func (a *ArrayList[T]) Delete(index int) (T, error) {
+	res, t, err := slice.Delete(a.vals, index)
+	if err != nil {
+		return t, err
+	}
+	a.vals = res
+	a.shrink()
+	return t, nil
+}
+
+// shrink 数组缩容
+func (a *ArrayList[T]) shrink() {
+	a.vals = slice.Shrink(a.vals)
+}
+
+func (a *ArrayList[T]) Len() int {
+	return len(a.vals)
+}
+
+func (a *ArrayList[T]) Cap() int {
+	return cap(a.vals)
+}
+
+func (a *ArrayList[T]) Range(fn func(index int, t T) error) error {
+	for key, value := range a.vals {
+		e := fn(key, value)
+		if e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+func (a *ArrayList[T]) AsSlice() []T {
+	res := make([]T, len(a.vals))
+	copy(res, a.vals)
+	return res
+}
